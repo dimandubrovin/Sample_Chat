b5299ec4d155b56bd678f346e4be914556dbbbae
dimandubrovin <dimandubrovin@gmail.com>
2016-11-14 20:55:45 +0700

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+	public InputField input;
+	public Text output;
+
+	private PlayerManager _currentPlayer;
+
+	public PlayerManager CurrentPlayer {
+		get { return _currentPlayer; }
+		set { _currentPlayer = value; }
+	}
+
+	public void Send ()
+	{
+		CurrentPlayer.Send (input.text);
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerManager : NetworkBehaviour
+{
+
+	GameManager _gameManager;
+
+	public void Start ()
+	{
+		_gameManager = FindObjectOfType<GameManager> ();
+
+		if (isLocalPlayer)
+			_gameManager.CurrentPlayer = this;
+	}
+
+	public void Send (string message)
+	{
+		CmdSend (Network.player.guid, message);
+	}
+
+	[Command]
+	public void CmdSend (string id, string message)
+	{
+		RpcSend (id, message);
+	}
+
+	[ClientRpc]
+	public void RpcSend (string id, string message)
+	{
+		_gameManager.output.text += id + ": " + message + System.Environment.NewLine;
+	}
+}
